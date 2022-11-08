c525f298ba98105a6319de014ee6443871577b6d
Chatheuis <rtromero.sp@gmail.com>
2022-11-08 16:40:50 -0300

    não é mais possível a abertura de mais de 1 forms do mesmo tipo

--- Estudio.cs
+++ Estudio.cs
@@ -30,7 +30,6 @@ namespace Estudio
         private void btnLogar_Click(object sender, EventArgs e)
         {
             int tipo = DAO_Conexao.login(txtLogin.Text, txtSenha.Text);
-            tipo = 1;
             if (tipo == 0)
                 MessageBox.Show("Usuário e/ou senha inválidos");
             if (tipo == 1)
@@ -65,44 +64,67 @@ namespace Estudio
 
         private void cadastrarAlunoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            CadastroAluno cad_aluno = new CadastroAluno();
-            cad_aluno.MdiParent = this;
-            cad_aluno.Show();
+            if (Application.OpenForms.OfType<CadastroAluno>().Count() == 0)
+            {
+                CadastroAluno cad_aluno = new CadastroAluno();
+                cad_aluno.MdiParent = this;
+                cad_aluno.Show();
+            }
+                
         }
 
         private void cadastrarLoginToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            CadastroUsuario cad_login = new CadastroUsuario();
-            cad_login.MdiParent = this;
-            cad_login.Show();
+            if (Application.OpenForms.OfType<CadastroUsuario>().Count() == 0)
+            {
+                CadastroUsuario cad_login = new CadastroUsuario();
+                cad_login.MdiParent = this;
+                cad_login.Show();
+            }
+                
         }
 
         private void excluirAlunoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Exclusao exclusao = new Exclusao();
-            exclusao.MdiParent = this;
-            exclusao.Show();
+            if (Application.OpenForms.OfType<Exclusao>().Count() == 0)
+            {
+                Exclusao exclusao = new Exclusao();
+                exclusao.MdiParent = this;
+                exclusao.Show();
+            }
+                
         }
 
         private void cadastrarModalidadeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Cad_Modalidade cad_modalidade = new Cad_Modalidade();
-            cad_modalidade.MdiParent = this;
-            cad_modalidade.Show();
+            if (Application.OpenForms.OfType<Cad_Modalidade>().Count() == 0)
+            {
+                Cad_Modalidade cad_modalidade = new Cad_Modalidade();
+                cad_modalidade.MdiParent = this;
+                cad_modalidade.Show();
+            }
+            
         }
 
         private void consultarModalidadeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Consulta_Modalidade consulta_modalidade = new Consulta_Modalidade();
-            consulta_modalidade.MdiParent = this;
-            consulta_modalidade.Show();
+            if (Application.OpenForms.OfType<Consulta_Modalidade>().Count() == 0)
+            {
+                Consulta_Modalidade consulta_modalidade = new Consulta_Modalidade();
+                consulta_modalidade.MdiParent = this;
+                consulta_modalidade.Show();
+            }
+                
         }
 
         private void excluirModalidadeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Exclusao_Modalidade exclusao_modalidade = new Exclusao_Modalidade();
-            exclusao_modalidade.MdiParent = this;
-            exclusao_modalidade.Show();
+            if (Application.OpenForms.OfType<Exclusao_Modalidade>().Count() == 0)
+            {
+                Exclusao_Modalidade exclusao_modalidade = new Exclusao_Modalidade();
+                exclusao_modalidade.MdiParent = this;
+                exclusao_modalidade.Show();
+            }                
         }
     }
 }
